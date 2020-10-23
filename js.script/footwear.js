b7bef2b681910bc61b79def0b9e93b75e68f00d2
vilmagogue <vilmagogue@yahoo.com>
2020-10-23 14:17:12 +1000

    updated

--- js.script/footwear.js
+++ js.script/footwear.js
@@ -1,35 +1,39 @@
 function footwear() {
-	 let shoes;
+    let shoes;
         //input use HTML combo box
         let weather = document.getElementById ('weather').value;
     
         //Processing
-    let shoes;
-    if (weather === 'hot'){
-         shoes = sandals;
+   if (weather==="shoes")
+		 shoes = weather;
+    else {
+			
+			if (weather ==="hot"){
+         shoes = "sandals";
     }
         else {
 
-         if (weather ==='rain') {
-                 shoes = boots;
+         if (weather ==="rain") {
+                 shoes = "galoshes";
     }
+
           else {
           
-        if (weather==='snow'){
-        shoes = galoshes;
-    }   
+        if (weather==="snow"){
+        shoes = "boots";
+    }  
+ 
       else {
           
-          if  (weather==='unknown'){
-          shoes = unknown;
+           shoes = "unknown";
     } 
+      }
+				}
 
-    }
-} 
     //output : Display the right shoes for the weather
-    document.getElementById('output').innerHTML = 'Have fun wearing' + shoes + "+"  + "!";
-
+					
+    document.getElementById('output').innerHTML = 'Have fun wearing ' + shoes   + ' in the '  + weather +  "!";
     }
-        
-   
 }
+
+
