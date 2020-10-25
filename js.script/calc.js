8ff18fa7e86916a82d14e025318c58a11cacb6c5
vilmagogue <vilmagogue@yahoo.com>
2020-10-25 11:59:20 +1000

    update

--- js.script/calc.js
+++ js.script/calc.js
@@ -1,56 +1,53 @@
 function calculate() {
     //input
-    let number1 = parseFloat(document.getElementById('number1').value); 
+    let num1 = parseFloat(document.getElementById('number1').value); 
     let operator = document.getElementById(operator).value;
-    let number2 = parseFloat(document.getElementById('number2').value);
-    let total = parseFloat(document.getElementById('answer').value);
+    let num2 = parseFloat(document.getElementById('number2').value);
+    let answer = parseFloat(document.getElementById('answer').value);
 	
 //apply selection combo box
 //apply switch : operator between two numbers
 
-let total;
+let answer;
 let message;
 switch (operator) {
     case '+':
-     number1 + number2===total;
-        if (total === 'answer'){
-            answer===total;
-        }
-            else {
-        if (total === number1 + number2){
-            message = "Correct! Good Job!";
+        answer == parseFloat(num1 + num2);
+        if (answer == answer){
+            message == "Correct! Good Job!";
         } 
         else {
-            message != "Incorrect! Try again!";
+            message !== "Incorrect! Try again!";
         }
-    }
+    
         break;  
-
+    
     case '-':
-        number1 - number2==total;
-        if (total == answer){
-            message = "Correct! Good Job!";
+        answer == parseFloat(num1 - num2);
+        if (answer == answer){
+            message == "Correct! Good Job!";
         } 
         else {
-            message = "Incorrect! Try again!";
+            message !== "Incorrect! Try again!";
         }
         break;
+        
     case '*':
-        total == parseFloat(number1 * number2);
-        if (total == answer){
-            message = "Correct! Good Job!";
+        answer == parseFloat(num1 * num2);
+        if (answer == answer){
+            message == "Correct! Good Job!";
         } 
         else {
-            message = "Incorrect! Try again!";
+            message !== "Incorrect! Try again!";
         }
         break;
     case '/':
-        total == parseFloat(number1 / number2);
-        if (total == answer){
-            message = "Correct! Good Job!";
+        answer == parseFloat(num1/ num2);
+        if (answer == answer){
+            message == "Correct! Good Job!";
         } 
         else {
-            message = "Incorrect! Try again!";
+            message !== "Incorrect! Try again!";
         }
         break;  
         default : 'none'       
@@ -59,6 +56,6 @@ switch (operator) {
 
 //Output:
 //Check answer
-document.getElementById('output').innerHTML = `The answer is${message}` ;
+document.getElementById('output').innerHTML = message ;
 }
         
