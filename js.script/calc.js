955a99dc3a18f7eae81e91b58b00611097f2e4dc
vilmagogue <vilmagogue@yahoo.com>
2020-10-26 14:52:18 +1000

    update

--- js.script/calc.js
+++ js.script/calc.js
@@ -1,47 +1,77 @@
-function simpleCalculator() {
+function calculator() {
     //input
     let number1 = parseFloat(document.getElementById('number1').value); 
-    let operator = document.getElementById('operators').value;
+    let operators = (document.getElementById('signOperators').value);
     let number2 = parseFloat(document.getElementById('number2').value);
-    let answer = parseFloat(document.getElementById('myAnswer').value);
-	let answerMath;
-    let message;    
+    let check = parseFloat(document.getElementById('myAnswer').value);
+	let calculate;
+       
 //apply selection combo box
 //apply switch : operator between two numbers
 //apply if..else conditions/comparisons
 
-switch (operator){
+switch (operators){
 
-    case 'add':
-        answerMath == number1 + number2  ;
+case '+':
+        calculate == parseFloat(number1 + number2)  ;
    break;
-case  'minus':
-    answerMath == number1 - number2;
+case  '-':
+    calculate == parseFloat(number1 - number2);
         break;
-case 'multiply':
-    answerMath== number1 * number2 ;
+case '*':
+    calculate== parseFloat(number1 * number2) ;
         break;
-case 'divide':
-    answerMath == number1 / number2;
+case '/':
+    calculate == parseFloat(number1 / number2);
         break;
     }
 
+let message;
+switch (operators) {
+case '+':
 
-    if (answer== number1+number2){
+    if (check == parseFloat(number1 + number2)) {
         message = 'It is 🏆 🏆correct!';
     }
-            else {
-            message = 'It is 😥 incorrect! Try Again!';
+
+    else {
+           
+         message = 'It is 😥 incorrect! Try Again!';
     }
-   
+  case '-'  :
+
+    if (check == parseFloat(number1 - number2)) {
+        message = 'It is 🏆 🏆correct!';
+    }
+
+    else {
+           
+         message = 'It is 😥 incorrect! Try Again!';
+    }
+    case '*':
+        if (check == parseFloat(number1 * number2)) {
+            message = 'It is 🏆 🏆correct!';
+        }
+    
+        else {
+               
+             message = 'It is 😥 incorrect! Try Again!';
+        }  
+        case '/':
+            if (check == parseFloat(number1 / number2)) {
+                message = 'It is 🏆 🏆correct!';
+            }
+        
+            else {
+                   
+                 message = 'It is 😥 incorrect! Try Again!';
+            }  
+}
 //Output
 //Check answer
 
-
 document.getElementById('output').innerHTML = message;
-
-    
-    }
+}
 
 
       
