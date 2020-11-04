27860cd31134ff29607fb3b6dfb066b342a0c79c
vilmagogue <vilmagogue@yahoo.com>
2020-11-04 19:36:12 +1000

    initial commit

--- alarm.js
+++ alarm.js
@@ -13,20 +13,20 @@ let dayOfWeek = now.getDay();
 let message;
        
 if (month==0||dayOfMonth==1||month==6||dayOfMonth==4||month==11||dayOfMonth==25||dayOfWeek==5||dayOfWeek==6||dayOfWeek==0){
-        message= 'sleepin';
+        alarm = 'sleepin';
 
 }
 else if (dayOfWeek==1||dayOfWeek==2||dayOfWeek==3||dayOfWeek==4||dayOfWeek==5){
-        message = 'Get Up!';
+        alarm = 'Get Up!';
 }  
 else{
-        message = 'Get Up!';
+        alarm = 'Get Up!';
 }   
         
 
     //output
 //get the right message :sleepin || get up
-document.getElementById('output').innerHTML = message ;
+document.getElementById('output').innerHTML = message + 'Get Up' + '+' + alarm + '+' + 'sleepin' + '+' + '!' ;
 
 }
 
