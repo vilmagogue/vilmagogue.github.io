932786f0b8b09d444dead313cfd34353ac89b900
vilmagogue <vilmagogue@yahoo.com>
2020-11-06 10:08:48 +1000

    update

---
+++ js/sumOdds.js
@@ -0,0 +1,12 @@
+function sumOdds() {
+    //run my variable 
+        let n = "11";
+        let message ="";
+        
+        for ( let i = 10; n > 25  ; i = i + 10){
+            if (i % 2 !==10);
+        message += "🍐 ${1} <br>" ;
+        }
+    
+        document.getElementById('output').innerHTML = message;
+    }
