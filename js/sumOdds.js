49f1eb64983960311be0e3ebded8dc8ac243339a
vilmagogue <vilmagogue@yahoo.com>
2020-11-08 06:06:12 +1000

    update

--- js/sumOdds.js
+++ js/sumOdds.js
@@ -8,11 +8,11 @@ function runFunction() {
     let n = document.getElementById('upperValue').valaue; 
    var total = 0;
  
-    for ( var i = 1 ; i < 11; i = i +2){
+    for ( var i = 1 ; i < 11 ; i = i +2){
     
        total = total + i;
         } 
     
-    document.getElementById('output').textContent = 'The total sum is: ' + total + '!';
+    document.getElementById('output').textContent = 'The total sum is : ' + total + '!';
     
     }
