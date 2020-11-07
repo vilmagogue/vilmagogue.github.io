27daca9c5b9446236e215243b36c7b98c59ea224
vilmagogue <vilmagogue@yahoo.com>
2020-11-08 05:51:07 +1000

    update

--- js/sumOdds.js
+++ js/sumOdds.js
@@ -1,18 +1,18 @@
 function runFunction() {
     //control variable
-    //determine initializer or the strting point
+    //determine initializer or the starting point
     //set conditions on how or when the loops end
     // loops incrementer and discrementer
     // var total as a collector
 
     let n = document.getElementById('upperValue').valaue; 
-   var total = '0';
+   var total = 0;
  
-    for ( var i = 1 ; i < 10 ; i = i +=2){
-    if ( i % 2 !==0){
+    for ( var i = 1 ; i < 11; i = i +2){
+    
        total = total + i;
         } 
-    }
-    document.getElementById('output').textContent = 'The total sum is: ' + total;
     
-}
+    document.getElementById('output').textContent = 'The total sum is: ' + total + '!';
+    
+    }
