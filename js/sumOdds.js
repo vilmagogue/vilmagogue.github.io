9c91dd4e6e711f99ff0ddc6d601b6a7a8952db34
vilmagogue <vilmagogue@yahoo.com>
2020-11-07 20:31:00 +1000

    initial commit

--- js/sumOdds.js
+++ js/sumOdds.js
@@ -1,15 +1,19 @@
-function sumFunctions() {
-    //run my variable
-   
-    
+function runFunction() {
+    //control variable
+    //determine initializer or the strting point
+    //st conditions when the the loops end
+    // loops incrementer and discrementer
+
+    let n = document.getElementById('upperValue').valaue; 
     let sum = '0';
  
-    for ( i = 1 ; i <= n  ; i = i+1){
-    let n = parseFloat(prompt(document.getElementById('enterNumber')).valaue); 
-        if ( n==3){
+    for ( i = 1 ; i < n  ; i++){
+    
+       output2 += output2 + i == i + 5;
             break;
         }  
-        sum += 4;
-    }
-    document.getElementById('output').textContent = sum + '🍊 ${n}';
+     
+    
+    document.getElementById('output').textContent = 'The total sum is: ' + sum;
     } 
+   
