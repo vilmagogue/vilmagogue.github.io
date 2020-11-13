896d7e32ba5c9a11ad3227d19e4427282f351837
vilmagogue <vilmagogue@yahoo.com>
2020-11-14 09:36:32 +1000

    updated Fixed

--- js/discount.js
+++ js/discount.js
@@ -1,25 +1,25 @@
 function logicalSolutions() {
         
         //input extract the dayOfWeek from the alarm clock
-        let subtotal =parseFloat(document.getElementById('subtotalInput').value);
-        var dayOfWeek = new Date().getDay();
+       let subtotal = parseInt(document.getElementById('subtotalnput').value);
+       var discount = .006;
         let message;
-        
+
         //Process
         //get the subtotal output for  Wednesday to get a customer discount
         //If statements-to compare sales is greater than $50/substractig 10 % and add 6% to the subtotal
         
-                
+     
         
-         if (subtotal <= 50 && dayOfWeek == 2 || subtotal >= 50 || dayOfWeek == 3) {
+         if (subtotal > 53.25 && dayOfWeek == 3||subtotal > 50 && dayOfWeek ==2) {
                  discount = '.10';
          }  
          
          else{
-                 discount = '50.80';
+                 discount = '.10';
          }
         
-        document.getElementById('output').innerHTMl = 'The subtotal amount of Customer discount with additional tax is 50.80' + message  + "!";
+        document.getElementById('output').innerHTMl = message + 'The subtotal amount of Customer discount with additional tax is 50.80' + subtotal +  "!";
         
         }
         
