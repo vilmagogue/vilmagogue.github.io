c954bc124badaaea03049c0ea0f5ebae299d535c
vilmagogue <vilmagogue@yahoo.com>
2020-11-21 11:35:03 +1000

    update initial commit

--- js/windChill.js
+++ js/windChill.js
@@ -1,24 +1,25 @@
 function doInputOutput() {
+    // do the input and output
     // get a temperature from the first text field and store the value into a variable named temperature
     // get a wind speed from the second text field and store the value into a variable named windspeed
     
-    let tempF = 25;
-    let windSpeed = 15;
-document.getElementById('output').innerHTML = windChill( tempF, windSpeed);
+    let tempF = parseInt(document.getElementById('temperature').value);
+    let windSpeed = parseInt(document.getElementById('windSpeed').value);
+document.getElementById('output').innerHTML = windChill (tempF, windSpeed);
  }
   
  // take a temperature in Fahrenheit as a parameter (tempF)
  // take a wind speed in miles per hour as a parameter (windSpeed)
  
- function windChill ( tempF, windSpeed ){
+ function windChill ( t, s ){
   // calculate the wind chill factor as a temperature in Fahrenheit  
-     f = 35.74 + 0.6215 * tempF - 35.75 * Math.pow (0.16) + 0.4275 * tempF -35.75 * Math.pow (0.16);
+  f = 35.74 + 0.6215 * t- 35.75 * Math.pow (s,0.16) + 0.4275 * t -35.75 * Math.pow (s,0.16);
+  
+  
  // call the function (windChill)
  // return the wind chill factor in Fahrenheit
 
-     windChill = (tempF, windSpeed)
-     return windChill;
+     return f;
  
-
   }
-  
+ 
