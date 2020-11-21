13571a4f8d830020d8de9279e15b6f6f1682c35b
vilmagogue <vilmagogue@yahoo.com>
2020-11-21 14:14:59 +1000

    update initial commit

--- js/windChill.js
+++ js/windChill.js
@@ -8,13 +8,13 @@ function doInputOutput() {
 document.getElementById('output').innerHTML = windChill (tempF, windSpeed);
  }
   
- // take a temperature in Fahrenheit as a parameter (tempF)
- // take a wind speed in miles per hour as a parameter (windSpeed)
+ // take a temperature in Fahrenheit as a parameter (t)
+ // take a wind speed in miles per hour as a parameter (s)
  
  function windChill ( t, s ){
   // calculate the wind chill factor as a temperature in Fahrenheit  
-  f = 35.74 + 0.6215 * t- 35.75 * Math.pow (s,0.16) + 0.4275 * t -35.75 * Math.pow (s,0.16);
   
+  f = 35.74 + 0.6215 *(t - 35.75)* (s,0.16) + 0.4275 *(t - 35.75) * (s,0.16);
   
  // call the function (windChill)
  // return the wind chill factor in Fahrenheit
