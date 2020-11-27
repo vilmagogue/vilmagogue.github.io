aa51260b0fdad13bf0dcd97d9d3616173633e0e5
vilmagogue <vilmagogue@yahoo.com>
2020-11-27 10:28:32 +1000

    updated initial commit

--- js/invest.js
+++ js/invest.js
@@ -1,18 +1,18 @@
 function doFV () {
-    let principal = parseInt(document.getElementById('principalAmount').value);
-    let rate = parseInt(document.getElementById('annualRate').value);
-    let numYears= parseInt(document.getElementById('numberYears').value);
-    let ppy= parseInt(document.getElementById('periodPerYear').value);
+    let principal = parseFloat(document.getElementById('principalAmount').value);
+    let rate = parseFloat(document.getElementById('annualRate').value);
+    let numYears= parseFloat(document.getElementById('numberYears').value);
+    let ppy= parseFloat(document.getElementById('periodPerYear').value);
 
     document.getElementById('output').innerHTML = "The future value is " + computeFutureValue (principal, rate, numYears, ppy ) 
-     + " for 10 years";
+     + " for 10 years " + "!";
 }
 function computeFutureValue( a, r, n, y ) {
 
 //The formula for computing the future value of an investment is
-// f = a * (1 + r)^n;
+// f = a * (1 + r)^y;
 //Compute the future value for 10 years ; 4 period per year;
 // where : a - principalAmount; r - annualRate; y - numberYears; n - periodPerYear
-f = (a * (( 1 + (r/n))**(n*y).toFixed(2)));
+f = (a * (( 1 + (r/y))**(n*y))).toFixed(2);
 return f;
 }
