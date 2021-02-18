574027c39f6f7f3eda2546a2290e0c3a0a6a3ab8
vilmagogue.github.io <gog20001@byui.edu>
2021-02-18 23:10:04 +1000

    updated

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,24 +1,12 @@
-function doInputOutput() {
-    // do the input and output
-    // get a temperature from the first text field and store the value into a variable named temperature
-    // get a wind speed from the second text field and store the value into a variable named windspeed
-    
-    let tempF = parseInt(document.getElementById('temperature').value);
-    let windSpeed = parseInt(document.getElementById('windSpeed').value);
-document.getElementById('output').innerHTML = windChill  (tempF, windSpeed) + " - The coolest temperature today🇬🇺🇺🇸!";
+function windChill(temp, speed){
+    let chill;
+    if ( temp <=50 && speed >3) {
+        chill = 35.74 + (0.6215 * temp)-(35.75 * Math.pow(speed,0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
 
+    }
+    else {
+        chill = "N/A";  
+    }
+    return chill.toFixed(2);
 }
- 
- function windChill ( tempF, windSpeed){
-     // take a temperature in Fahrenheit as a parameter (tempF)
- // take a wind speed in miles per hour as a parameter (windSpeed)
-  
- //Processing: calculate the wind chill factor as a temperature in Fahrenheit  
-  
-  f = 35.74 + (0.6215 * tempF)- (35.75 * windSpeed ** 0.16) + (0.4275 * tempF * windSpeed ** 0.16);
- 
- // return the wind chill factor in Fahrenheit
-
-     return f;
- 
-  }
+document.getElementById("windChill").innerHTML = windChill();
