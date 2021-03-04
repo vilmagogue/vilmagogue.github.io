3d147bc4d48a6cb58353654f1de434ccf6a2e23d
vilmagogue.github.io <gog20001@byui.edu>
2021-03-04 23:18:03 +1000

    init commit

---
+++ lesson9/js/prophets.js
@@ -0,0 +1,22 @@
+const requestURL = 'byui-cit230.github.io/canvas-referenced/latter-day-prophets.json';
+fetch(requestURL)
+  .then(function (response) {
+    
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    for (let i = 0; i < prophets.length; i++ ) {
+  });
+ 
+  const prophets = jsonObject['prophets'];
+  
+  let card = document.createElement('section');
+let h2 = document.createElement('h2');
+
+h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+
+card.appendChild(h2);
+
+document.querySelector('div.cards').appendChild(card);
+
