6fc4e8011a061526cdff8277ea11b3331da3a6cd
vilmagogue.github.io <gog20001@byui.edu>
2021-03-06 05:25:52 +1000

    updated

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -7,14 +7,18 @@ fetch(requestURL)
     const prophets = jsonObject['prophets'];
     for (let i = 0; i < prophets.length; i++ ) {
 
-
 let card = document.createElement('section');
 let h2 = document.createElement('h2');
+let cardP = document.createElement('p');
+let image = document.createElement('img');
 
 h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+cardP.textContent ="Born:" + "" + prophets[i].birthdate;
+cardP.textContent ="Place:" + "" + prophets[i].birthplace;
 
-card.appendChild(h2);
+image.setAttribute("alt", prophets[i].name + "" + prophets[i]. lastname + "" + "-" + "")
 
 document.querySelector('div.cards').appendChild(card);
+
     }
 }); 
