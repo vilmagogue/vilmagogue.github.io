297901269c1b47239cdb4eeaeed645756f4327c2
vilmagogue.github.io <gog20001@byui.edu>
2021-03-06 12:55:30 +1000

    update

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -9,16 +9,24 @@ fetch(requestURL)
 
 let card = document.createElement('section');
 let h2 = document.createElement('h2');
-let cardP = document.createElement('p');
-let image = document.createElement('img');
+let bDay = document.createElement('p');
+let bPlace = document.createElement('p');
+let image= document.createElement('img');
 
 h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-cardP.textContent ="Born:" + "" + prophets[i].birthdate;
-cardP.textContent ="Place:" + "" + prophets[i].birthplace;
+bDay.textContent ="Born:" + "" + prophets[i].birthdate;
+bPlace.textContent ="Born:" + "" + prophets[i].birthdate;
+image.setAttribute('src', prophets[i].imageurl);
+image.setAttribute("alt", prophets[i].name + "" + prophets[i]. lastname + "" + "-" + "" + prophets[i].order);
 
-image.setAttribute("alt", prophets[i].name + "" + prophets[i]. lastname + "" + "-" + "")
+
+card.append(h2);
+card. appendChild(bday);
+card.appendChild(bPlace);
+card.append(image);
 
 document.querySelector('div.cards').appendChild(card);
 
     }
 }); 
+
