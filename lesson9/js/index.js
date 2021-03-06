afe902ca56154c43b29b82904497fd0cee18363e
vilmagogue.github.io <gog20001@byui.edu>
2021-03-06 11:07:11 +1000

    update

---
+++ lesson9/js/index.js
@@ -0,0 +1,27 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const towns= jsonObject['townData'];
+    for (let i = 0; i < towns.length; i++ ) {
+
+let towns= document.createElement('section');
+let h2 = document.createElement('h2');
+let townP = document.createElement('p');
+let image = document.createElement('img');
+
+h2.textContent = towns[i].name + ' ' + towns[i].lastname;
+townP.textContent ="motto:" + "" + prophets[i].birthdate;
+townP.textContent ="yearFounded:" + "" + prophets[i].birthplace;
+townP.textContent ="currentPopulation:" + "" + prophets[i].birthplace;
+townP.textContent ="averageRainfall:" + "" + prophets[i].birthplace;
+townP.textContent ="Events:" + "" + prophets[i].birthplace;
+
+image.setAttribute("alt", prophets[i].name + "" + prophets[i]. lastname + "" + "-" + "")
+
+document.querySelector('div.cards').appendChild(card);
+
+    }
+}); 
