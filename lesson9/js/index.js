ac0d61c43104abc0ec83cb5187746fab168ff01d
vilmagogue.github.io <gog20001@byui.edu>
2021-03-13 23:14:15 +1000

    update

--- lesson9/js/index.js
+++ lesson9/js/index.js
@@ -1,110 +1,52 @@
-
-const images = document.querySelectorAll("[data-src]");
-
-function preloadImage(img) {
-  const src = img.getAttribute("data-src");
-  if (!src) {
-    return;
-  }
-
-  img.src = src;
-  img.removeAttribute("data-src");
-}
-
-const imgOptions = {
-  threshold: 1,
-  rootMargin: "0px 0px 200px 0px",
-};
-
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return;
-    } else {
-      preloadImage(entry.target);
-      imgObserver.unobserve(entry.target);
-    }
-  });
-}, imgOptions);
-
-images.forEach((image) => {
-  imgObserver.observe(image);
-});
-
-
-/* ============= Brother Blazzar function for range rating==============*/
-function adjustRating(rating) {
-  document.getElementById("ratingvalue").innerHTML = rating;
-}
-
 /* ============= cards code for home page ========================*/
 /* Display the individual town data with the motto, year founded, population, and annual rainfall.*/
 // set the JSON source URL
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-        //console.table(jsonObject);  // temporary checking for valid response and data parsing
-        const towns = jsonObject['towns'];
+  //  console.table(jsonObject)//
+   // temporary checking for valid response and data parsing//
 
-        const cards = document.querySelector(".cards")
-        // select output location
-        towns.forEach( town => {
-          let divText = document.createElement("div");
-          let card = document.createElement("section");
-          let h2 = document.createElement("h2");
-          let h5 = document.createElement("h5");
-          let p2 = document.createElement("p");
-          let p3 = document.createElement("p");
-          let p4 = document.createElement("p");
-          let townimg = document.createElement("img");
-          
-          if (town.name == "Preston" || town.name == "Fish Haven" || town.name == "Soda Springs") {
-            // template literals
-            h2.textContent = `${town.name}`;
-            h2.style.margin = '2px 0'
-            h5.textContent = `${town.motto}`;
-            h5.style.margin = '2px 0';
-            p2.textContent = `Year Founded: ${town.yearFounded}`;
-            p3.textContent = `Population: ${town.population}`;
-            p4.textContent = `Annual Rain Fall: ${town.averageRainfall}`;
-            pMargin = '1rem 0 0 0' // variable to hold margin of paragraphs
-            p2.style.margin = '2rem 0 0 0'
-            p3.style.margin = pMargin
-            p4.style.margin = pMargin
-            divText.setAttribute('class', "content");
+   const prophets = jsonObject['prophets'];
+// loop variables in the array//
+   for (let i = 0; i < towns.length; i++ ) {
 
-            townimg.setAttribute('src' , `images/${town.photo}`);
-            townimg.setAttribute("alt", `Prophet ${town.name} photo`);
-            townimg.setAttribute("loading", "lazy")
-            townimg.style.boxShadow = '0 0 30px #333';
-            townimg.style.width = '200px';
-            
+    // declare - output of the declared  section//
 
-            divText.append(h2);
-            divText.append(h5);
-            divText.append(p2);
-            divText.append(p3);
-            divText.append(p4);
-            card.append(divText);
-            card.append(townimg);
-            cards.append(card);
-          }
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let motto = document.createElement('p');
+    let yearFounded = document.createElement('p');
+    let currentPopulation = document.createElement("p");
+    let annualRainfall = document.createElement("p")
+    let townPhoto = document.createElement('img');
 
-          function addingOdds(altVariable){
-            divText.setAttribute('class', "data");
-            if(altVariable%2==0){
-                townimg.setAttribute('class', "derecha");
-                divText.setAttribute('class', "data2");
-            }
-          }
+    // set output string, set all attributes declared from HTML/card //
+h2.textContent = town[i].name + " " + towns[i].name;
+motto.textContent = "motto :" + " " + towns[i].motto;
+yearFounded.textContent ="yearFounded :" + " " + towns[i].yearFounded;
+currentPopulation = "currentPopulation :" + " " + towns[i].currentPopulation;
+averageRainfall = "averageRainfall :" + " " + towns[i].averageRainfall;
 
-      });
 
-      
+townPhoto.setAttribute('src' , `images/${town.photo}`);
+            townPhoto.setAttribute("alt", `town ${town.name} photo`);
+            townPhoto.setAttribute("loading", "lazy")
+            townPhoto.style.boxShadow = '0 0 30px #333';
+            townPhoto.style.width = '200px';
 
-  });
-
+card.append(h2);
+card.appendChild(name);
+card.appendChild(motto);
+card.append(yearFounded);
+card.append(currentPopulation);
+card.append(averageRainful);
+document.querySelector("div.cards").appendChild(card);
+   }
+});
+  
